d32d393ac1926c5e8fd16db6d2f88133e7e9d8f2
Felipe González <felipe.gonzalez@unir.net>
2024-05-19 19:42:20 -0500

    Actualizado unión de dataframes

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ from src.data_preparation import load_data
 
 def main():
     # Load and clean data
-    train_data = load_data('data/TrainingDatasets/TREC-2017-LiveQA-Medical-Train-1.xml')
+    data_paths = ['data/TrainingDatasets/TREC-2017-LiveQA-Medical-Train-1.xml', 'data/TrainingDatasets/TREC-2017-LiveQA-Medical-Train-2.xml']
+    train_data = load_data(data_paths)
     
     # Exploratory data analysis
-    print(train_data[["subject", "focus", "type", "answer"]].head(15))
-    print(train_data[["subject", "message", "focus", "type", "answer"]].head(15))
-
+    print(train_data[["question_id","focus", "answer"]].head(45))
 
 if __name__ == "__main__":
     main()
